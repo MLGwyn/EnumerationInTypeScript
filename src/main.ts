9f5c8c6f23c7f34f0f051f23fc2b97cedeaf9c34
Melissa Gwyn <melissagwyn@gmail.com>
2023-08-09 12:42:23 -0400

    1st commit

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,9 @@
-import "./style.css";
+import './style.css'
+
+const colors = ['red', 'green', 'blue']
+
+function logSomeColor(color: string, index: number) {
+  console.log(`The color at position ${index} is ${color}`)
+}
+
+colors.forEach(logSomeColor)
